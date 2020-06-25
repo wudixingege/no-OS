ef258c2a536e885cd1f6255a228b049481fa4b04
Mihail Chindris <mihail.chindris@analog.com>
2020-06-25 13:28:20 +0300

    iio:libraries: Replace iio_interfaces with list
    iio_interfaces is actually a list. Use list.h instead of implement inserts and searches
    
    Signed-off-by: Mihail Chindris <mihail.chindris@analog.com>

--- projects/iio_demo/src/main.c
+++ projects/iio_demo/src/main.c
@@ -209,6 +209,7 @@ int main(void)
 		.ddr_base_addr = OUTPUT_BASE_ADDRESS,
 	};
 
+	iio_demo_out_init_par.iio_desc = iio_desc;
 	status = iio_demo_init(&iio_demo_out_desc, &iio_demo_out_init_par);
 	if (status < 0)
 		return status;
@@ -219,6 +220,7 @@ int main(void)
 		.ddr_base_addr = INPUT_BASE_ADDRESS,
 	};
 
+	iio_demo_in_init_par.iio_desc = iio_desc;
 	status = iio_demo_init(&iio_demo_in_desc, &iio_demo_in_init_par);
 	if (status < 0)
 		return status;
