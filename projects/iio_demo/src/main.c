b6db1daa6a4c07eee8826e45a72b97043b390a4c
Mihail Chindris <mihail.chindris@analog.com>
2020-06-25 11:54:36 +0300

    projects:iio_demo: Add support for aducm
    Add support for aducm3029 in ii_demo project
    
    Signed-off-by: Mihail Chindris <mihail.chindris@analog.com>

--- projects/iio_demo/src/main.c
+++ projects/iio_demo/src/main.c
@@ -41,12 +41,42 @@
 /***************************** Include Files **********************************/
 /******************************************************************************/
 
+#include "app_config.h"
+
 #ifdef XILINX_PLATFORM
 #include <xparameters.h>
 #include <xil_cache.h>
 #include <xil_cache.h>
+
+#define OUTPUT_BASE_ADDRESS DAC_DDR_BASEADDR
+#define OUTPUT_SIZE 		10000
+
+#define INPUT_BASE_ADDRESS	ADC_DDR_BASEADDR
+/* 400 * 8 * 2 = 6400‬ Default number of samples requested on a capture */
+#define	INPUT_SIZE			10000
+
 #endif // XILINX_PLATFORM
-#include "app_config.h"
+
+
+#ifdef ADUCM_PLATFORM
+
+#include <sys/platform.h>
+#include "adi_initialize.h"
+#include <drivers/pwr/adi_pwr.h>
+
+#define MAX_BUFF_SIZE		3000
+
+static uint8_t in_buff[MAX_BUFF_SIZE];
+static uint8_t out_buff[MAX_BUFF_SIZE];
+
+#define OUTPUT_BASE_ADDRESS (uint32_t)out_buff
+#define OUTPUT_SIZE 		MAX_BUFF_SIZE
+
+#define INPUT_BASE_ADDRESS	(uint32_t)in_buff
+#define	INPUT_SIZE			MAX_BUFF_SIZE
+
+#endif
+
 #include "parameters.h"
 #include "error.h"
 #include "iio_app.h"
@@ -79,6 +109,21 @@ static ssize_t iio_uart_write(const char *buf, size_t len)
 static ssize_t iio_uart_read(char *buf, size_t len)
 {
 	return uart_read(uart_desc, (uint8_t *)buf, len);
+}
+
+int32_t platform_init() {
+#ifdef ADUCM_PLATFORM
+	if (ADI_PWR_SUCCESS != adi_pwr_Init())
+		return FAILURE;
+
+	if (ADI_PWR_SUCCESS != adi_pwr_SetClockDivider(ADI_CLOCK_HCLK, 1u))
+		return FAILURE;
+
+	if (ADI_PWR_SUCCESS != adi_pwr_SetClockDivider(ADI_CLOCK_PCLK, 1u))
+		return FAILURE;
+	adi_initComponents();
+#endif
+	return SUCCESS;
 }
 
 /***************************************************************************//**
@@ -120,35 +165,23 @@ int main(void)
 	/* IRQ instance. */
 	struct irq_ctrl_desc *irq_desc;
 
+
+	status = platform_init();
+	if (IS_ERR_VALUE(status))
+		return status;
+
 #ifdef XILINX_PLATFORM
 	/* Xilinx platform dependent initialization for IRQ. */
-	struct xil_irq_init_param xil_irq_init_par;
-
-	/* Xilinx platform dependent initialization for UART. */
-	struct xil_uart_init_param xil_uart_init_par;
-
-	xil_irq_init_par = (struct xil_irq_init_param ) {
+	struct xil_irq_init_param platform_irq_init_par = {
 #ifdef XPAR_INTC_SINGLE_DEVICE_ID
 		.type = IRQ_PL,
 #else
 		.type = IRQ_PS,
 #endif
 	};
-#endif // XILINX_PLATFORM
 
-	irq_init_param = (struct irq_init_param ) {
-		.irq_ctrl_id = INTC_DEVICE_ID,
-#ifdef XILINX_PLATFORM
-		.extra = &xil_irq_init_par,
-#endif // XILINX_PLATFORM
-	};
-
-	status = irq_ctrl_init(&irq_desc, &irq_init_param);
-	if(status < 0)
-		return status;
-
-#ifdef XILINX_PLATFORM
-	xil_uart_init_par = (struct xil_uart_init_param) {
+	/* Xilinx platform dependent initialization for UART. */
+	struct xil_uart_init_param platform_uart_init_par = {
 #ifdef XPAR_XUARTLITE_NUM_INSTANCES
 		.type = UART_PL,
 #else
@@ -157,14 +190,33 @@ int main(void)
 		.irq_id = UART_IRQ_ID,
 		.irq_desc = irq_desc,
 	};
+
 #endif // XILINX_PLATFORM
 
+#ifdef ADUCM_PLATFORM
+	/* Dummy value for Aducm platform dependent initialization for IRQ. */
+	int32_t platform_irq_init_par = 0;
+
+	/* Aducm platform dependent initialization for UART. */
+	struct aducm_uart_init_param platform_uart_init_par = {
+			.parity = UART_NO_PARITY,
+			.stop_bits = UART_ONE_STOPBIT,
+			.word_length = UART_WORDLEN_8BITS };
+#endif // ADUCM_PLATFORM
+
+	irq_init_param = (struct irq_init_param ) {
+		.irq_ctrl_id = INTC_DEVICE_ID,
+		.extra = &platform_irq_init_par
+	};
+
+	status = irq_ctrl_init(&irq_desc, &irq_init_param);
+	if(status < 0)
+		return status;
+
 	uart_init_par = (struct uart_init_param) {
-		.baud_rate = 921600,
 		.device_id = UART_DEVICE_ID,
-#ifdef XILINX_PLATFORM
-		.extra = &xil_uart_init_par,
-#endif // XILINX_PLATFORM
+		.baud_rate = UART_BAUDRATE,
+		.extra = &platform_uart_init_par
 	};
 
 	status = uart_init(&uart_desc, &uart_init_par);
@@ -192,7 +244,7 @@ int main(void)
 	iio_demo_out_init_par = (struct iio_demo_init_param) {
 		.name = demo_device_output,
 		.num_channels = 4,
-		.ddr_base_addr = DAC_DDR_BASEADDR,
+		.ddr_base_addr = OUTPUT_BASE_ADDRESS,
 	};
 
 	status = iio_demo_init(&iio_demo_out_desc, &iio_demo_out_init_par);
@@ -202,7 +254,7 @@ int main(void)
 	iio_demo_in_init_par = (struct iio_demo_init_param) {
 		.name = demo_device_input,
 		.num_channels = 4,
-		.ddr_base_addr = DAC_DDR_BASEADDR,
+		.ddr_base_addr = INPUT_BASE_ADDRESS,
 	};
 
 	status = iio_demo_init(&iio_demo_in_desc, &iio_demo_in_init_par);
